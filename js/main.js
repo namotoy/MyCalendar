c41a19500f61ce64ea75cafec8a3e42adbd80ee1
namotoy <namotopet0615@gmail.com>
2022-05-24 12:43:04 +0900

    前月分の日付取得

--- js/main.js
+++ js/main.js
@@ -1,15 +1,31 @@
 'use strict';
+
 console.clear();
 
 {
-const year = 2022;
-const month = 4;
+  const year = 2022;
+  const month = 4;
+
+  function getCalendarHead() {
+    const dates = [];
+    const d = new Date(year, month, 0).getDate();
+    const n = new Date(year, month, 1).getDay();
+
+  for (let i = 0; i < n; i++) {
+    dates.unshift({
+      date: d - i,
+      isToday: false,
+      isDisabled: true,
+    });
+  }
+  console.log(dates);
+}
 
 function getCalendarBody(){
   const dates = [];
   const lastDate = new Date(year, month + 1, 0).getDate();
 
-  for (let i = 1; i <= lastDate; i++){
+  for (let i = 1; i <= lastDate; i++) {
     dates.push({
       date: i,
       isToday: false,
@@ -18,5 +34,7 @@ function getCalendarBody(){
   }
   console.log(dates);
 }
-  getCalendarBody();
+
+getCalendarHead();
+// getCalendarBody();
 }
