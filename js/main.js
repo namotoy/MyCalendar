fc15fda961e086940bf890c8a43b7dea40d749ec
namotoy <namotopet0615@gmail.com>
2022-05-24 12:07:30 +0900

    Javascript記述

---
+++ js/main.js
@@ -0,0 +1,22 @@
+'use strict';
+console.clear();
+
+{
+const year = 2022;
+const month = 4;
+
+function getCalendarBody(){
+  const dates = [];
+  const lastDate = new Date(year, month + 1, 0).getDate();
+
+  for (let i = 1; i <= lastDate; i++){
+    dates.push({
+      date: i,
+      isToday: false,
+      isDisabled: false,
+    });
+  }
+  console.log(dates);
+}
+  getCalendarBody();
+}
